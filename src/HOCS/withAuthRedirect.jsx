0dba97d7c6bde605fe0b5c5e32575d62807bef8c
unknown <kirildim16@gmail.com>
2019-12-26 23:17:34 +0200

    refactoring

--- src/HOCS/withAuthRedirect.jsx
+++ src/HOCS/withAuthRedirect.jsx
@@ -9,11 +9,7 @@ const mapStateToProps = state => ({
 export const withAuthRedirect = Component => {
     class RedirectComponent extends React.Component {
         render() {
-            if(this.props.isAuth === false) {
-                const savedAuthInfoByLocalStorage = JSON.parse(localStorage.getItem('auth'))
-                return savedAuthInfoByLocalStorage.isAuth && <Redirect to='/login'/>
-                
-            }
+            if(!this.props.isAuth) return <Redirect to='/login'/>
             return <Component {...this.props}/>
         }
     }
